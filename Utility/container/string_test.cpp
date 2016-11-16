f504e72775c7bda4abbde7f4342416c58bfc7303
TimGl105 <tim.gloeckner1@web.de>
2016-11-16 12:23:17 +0100

    String- and Vector testfiles added

---
+++ Utility/container/string_test.cpp
@@ -0,0 +1,12 @@
+#include "C:\Users\Tim\Dropbox\GitHub\lexer\Utility\container\string.h"
+#include <iostream>
+
+
+int main() {
+
+	char* fuck_this = "fuck this";
+	String string_1(fuck_this);
+	String string_2(string_1);
+	
+
+}
