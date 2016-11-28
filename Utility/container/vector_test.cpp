8ce077de1da0409d27c96f1abc02716bf89d0709
TimGl105 <tim.gloeckner1@web.de>
2016-11-28 01:29:51 +0100

    Vector testfile simply modified

--- Utility/container/vector_test.cpp
+++ Utility/container/vector_test.cpp
@@ -6,6 +6,7 @@
  */
 #include "vector.h"
 #include <iostream>
+#include <string>
 #include "string.h"
 
 void t_out(bool t) { std::cout << "Test: " << t << std::endl; }
@@ -14,9 +15,9 @@ void s_out(const String& string) { std::cout << string.c_str() << std::endl; }
 
 int main() {
 
-	// CONTAINS != CEND() und STRING += und CLEAR Vector<value_type>&
+	// 
 	
-	//done 
+	//Testcase 1: done 
 	Vector<int> random_vec(std::size_t(100), 1);
 	random_vec[99] = 99;
 	const Vector<int> const_random_vec = random_vec;
@@ -28,7 +29,7 @@ int main() {
 	}
 	
 	
-	//done
+	//Testcase 2: done
 	Vector<String> string_vec(9, "world");
 	string_vec.push_back("end2");
 	string_vec.push_back("end1");
@@ -39,13 +40,13 @@ int main() {
 	vector_vec.push_back(string_vec);
 	
 
-	//done
+	//Testcase 3: done
 	//std::cout << vector_vec[10].pop_back().c_str() << std::endl;
 	t_out(0 == strcmp("end1", vector_vec[10].pop_back().c_str()));
 	//std::cout << vector_vec.pop_back().pop_back().c_str() << std::endl;
 	t_out(0 == strcmp("end2", vector_vec.pop_back().pop_back().c_str()));
 	
-	//
+	//Testcase 4: 
 	Vector<String> str_vec(3, "world");
 	Vector<String> str_vec2;
 	str_vec.push_back("end");
@@ -58,7 +59,7 @@ int main() {
 		s_out(str_vec2.pop_back());
 	}
 
-	//done
+	//Testcase 5: done
 	vector_vec.clear();
 	//std::cout << vector_vec.size() << std::endl;
 	t_out(vector_vec.size() == 0);
@@ -66,12 +67,12 @@ int main() {
 	std::cout << "free capacity: " << vector_vec.free_capacity() << std::endl;
 	t_out(vector_vec.capacity() == vector_vec.free_capacity());
 	
-	//failure
+	//Testcase 6:
 	Vector<String> str_ult;
 	/*for (int i = 4; i > -2; i--) {
 		str_ult.push_back(std::to_string(i).c_str());
 	}*/
-	str_ult.push_back("4");
+	/*str_ult.push_back("4");
 	str_ult.push_back("3");
 	str_ult.push_back("2");
 	str_ult.push_back("1");
@@ -79,35 +80,32 @@ int main() {
 	str_ult.push_back("-1");
 	
 	if (str_ult.contains("-1")) {
-		str_ult.insert(str_ult.rend() + 1, str_ult.rbegin() + 1, str_ult.rend() - 1);
+		str_ult.insert(str_ult.rend() + 1, str_ult.begin() + 1, str_ult.end() - 1);
 	}
 	t_out(str_ult.size() == 11);
 	std::cout << "capacity: "<< str_ult.capacity() << std::endl;
 	std::cout << "free capacity: " << str_ult.free_capacity() << std::endl;
 	for (int i = str_ult.size(); i > 0; i--) {
 		s_out(str_ult.pop_back());
-	}
+	}*/
 	
-	//done
+	//Testcase 7: done
 	Vector<int> v_empty;
 	t_out(0 == v_empty.size());
 	t_out(16 == v_empty.capacity());
 	t_out(16 == v_empty.free_capacity());
-	//done
+	//Testcase 8: done
 	Vector<int> v_0(0);
 	t_out(0 == v_0.size());
 	t_out(0 == v_0.capacity());
 	t_out(0 == v_0.free_capacity());
 	
+	//Testcase 9: 
 	Vector<int*> v_1(1, nullptr);
-
-	//v_vector.push_back(v_string);
-	
-	//std::cout << v_string[2] << std::endl;
-	//std::cout << *v_vector[2].find("world") << std::endl;
+	t_out(NULL == v_1.find(nullptr));
 
 	
-	// Million element
+	//Testcase 10:
 	Vector<String> mill_vec(std::size_t(1000000));
 	Vector<String> p1_vec(std::size_t(250000), "p1");
 	Vector<String> p2_vec(std::size_t(250000), "p2");
@@ -122,17 +120,18 @@ int main() {
 	mill_vec.insert(mill_vec.begin(), p1_vec.begin(), p1_vec.end());
 	mill_vec.insert(mill_vec.begin() + 250000, p2_vec.begin(), p2_vec.end());
 	
-	std::Vector<String> mill_vec_std(std::size_t(1000000));
-	std::Vector<String> p1_vec_std(std::size_t(250000), "p1");
-	std::Vector<String> p2_vec_std(std::size_t(250000), "p2");
-	std::Vector<String> p3_vec_std(std::size_t(250000), "p3");
-	std::Vector<String> p4_vec_std(std::size_t(250000));
+	
+	std::vector<String> mill_vec_std(std::size_t(1000000));
+	std::vector<String> p1_vec_std(std::size_t(250000), "p1");
+	std::vector<String> p2_vec_std(std::size_t(250000), "p2");
+	std::vector<String> p3_vec_std(std::size_t(250000), "p3");
+	std::vector<String> p4_vec_std(std::size_t(250000));
 	for (int i = 1; i <= 250000; i++) {
 		p4_vec_std.push_back(std::to_string(i).c_str());
 	}
 	
-	mill_vec.insert(mill_vec_std.begin(), p4_vec_std.begin(), p4_vec_std.end());
-	mill_vec.insert(mill_vec_std.begin(), p3_vec_std.begin(), p3_vec_std.end());
-	mill_vec.insert(mill_vec_std.begin(), p1_vec_std.begin(), p1_vec_std.end());
-	mill_vec.insert(mill_vec_std.begin() + 250000, p2_vec_std.begin(), p2_vec_std.end());
+	mill_vec.insert<String>(mill_vec_std.begin(), p4_vec_std.begin(), p4_vec_std.end());
+	mill_vec.insert<String>(mill_vec_std.begin(), p3_vec_std.begin(), p3_vec_std.end());
+	mill_vec.insert<String>(mill_vec_std.begin(), p1_vec_std.begin(), p1_vec_std.end());
+	mill_vec.insert<String>(mill_vec_std.begin() + 250000, p2_vec_std.begin(), p2_vec_std.end());
 }
